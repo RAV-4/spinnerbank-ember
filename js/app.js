4ce6477fd2ecf5f5e538d80f60284d61f09a18ba
unknown <camilo.guerra.gonzalez@gmail.com>
2015-10-08 18:41:47 -0500

    Se hizo en el index.html un select-box en html con un style incrustado, pero en posteriores intacias se espera utulizar components tempatles para hacer el mismo select-box

--- js/app.js
+++ js/app.js
@@ -2,12 +2,39 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.route('first-route',{path:'first-route'})//la ruta y el nombre del path no tiene que ser el mismo
+  
 });
 
-App.model = ['red', 'yellow', 'blue'];
-App.set('model',['red','yellow','blue','green']);
-/*App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
+//App.FirstRoute = Ember.Route.extend({
+
+
+	
+//});
+
+App.FirstRouteController = Ember.Controller.extend({
+
+
+ 
+actions:{
+
+		click_handler: function(){
+
+			alert('click');
+		}
+	}
+
+
+});
+
+App.IndexRoute = Ember.Route.extend({
+ model: function() {
+    return[
+
+     
+
+    ] ;
   }
-});*/
+
+
+});
