3ab91c7b5d01b0661d50ff876f202be279326cbc
unknown <camilo.guerra.gonzalez@gmail.com>
2015-10-25 10:34:40 -0500

    Se hace estructura del metodo para que salga una alerta de error con el idCliente

--- app/controllers/clientes.js
+++ app/controllers/clientes.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+actions: {
 
-  
-  
-  
+  compidClient: function(){
+  let idClient = this.get('idClient');
+  if (idClient){
+    window.alert(idClient);
+    }
+
+  }
+}
+
+
+
 });
--- app/routes/clientes.js
+++ app/routes/clientes.js
@@ -1,9 +1,8 @@
-
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    
+
     return $.getJSON(
     'http://spinnerbank-api-external.herokuapp.com/productos/1936941186',
     function(data){
